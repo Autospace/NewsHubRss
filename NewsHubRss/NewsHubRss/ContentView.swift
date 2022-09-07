00010bb4a11618ba0bfa572ba83b724129d675e9
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2022-09-07 23:18:02 +0200

    Initial commit

---
+++ NewsHubRss/NewsHubRss/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsHubRss
+//
+//  Created by Alex Mostovnikov on 7/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("NewsHubRss")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsHubRss/NewsHubRss/NewsHubRssApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsHubRssApp.swift
+//  NewsHubRss
+//
+//  Created by Alex Mostovnikov on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsHubRssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
