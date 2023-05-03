9d0d5ae80b5ac75729af738396c1c3d7ae7799a2
TheMALX <mostovnikov.alex@gmail.com>
2023-05-03 19:58:34 +0200

    Add functionality to scan entered URL to find all feeds by this URL and show them on the AddFeedView

--- NewsHubRss/NewsHubRss/Services/Networking.swift
+++ NewsHubRss/NewsHubRss/Services/Networking.swift
@@ -24,13 +24,30 @@ struct Networking {
 
             if let data = data, let stringData = String(data: data, encoding: .utf8) {
                 completion(.success(stringData))
-                return
             } else if let error = error {
                 completion(.failure(error))
             } else {
                 completion(.failure(NetworkError()))
             }
         }
+        dataTask.resume()
+    }
+
+    static func detectRssFeed(by url: URL, completion: @escaping (_ isRSS: Bool) -> Void) {
+        let session = URLSession(configuration: .ephemeral)
+        var request = URLRequest(url: url)
+        request.httpMethod = "Head"
+        let dataTask = session.dataTask(with: request) { (_, response, _) in
+            guard let httpResponse = response as? HTTPURLResponse,
+                httpResponse.statusCode == 200,
+                let contentType = httpResponse.allHeaderFields["Content-Type"] as? String else {
+                completion(false)
+                return
+            }
+
+            completion(contentType.hasPrefix("application/rss+xml") || contentType.hasPrefix("application/xml"))
+        }
+
         dataTask.resume()
     }
 }
--- NewsHubRss/NewsHubRss/Views/AddFeedView.swift
+++ NewsHubRss/NewsHubRss/Views/AddFeedView.swift
@@ -6,48 +6,52 @@
 //
 
 import SwiftUI
+import SwiftSoup
 
 struct AddFeedView: View {
     @State private var feedUrl = ""
     @State private var isLoading = false
     @State private var hasError = false
     @State private var errorText = ""
+    @State private var foundRssFeeds: [String] = []
 
     var body: some View {
-        Form {
-            Text(L10n.AddNewFeed.instruction)
-            TextField(L10n.AddNewFeed.TextField.placeholder, text: $feedUrl)
-                .frame(height: 44)
-                .autocapitalization(.none)
-                .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(hasError ? Color.red : Color.gray)
-                )
-                .disableAutocorrection(true)
-                .onChange(of: feedUrl) { _ in
-                    hasError = false
+        VStack {
+            Form {
+                Text(L10n.AddNewFeed.instruction)
+                TextField(L10n.AddNewFeed.TextField.placeholder, text: $feedUrl)
+                    .frame(height: 44)
+                    .autocapitalization(.none)
+                    .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(hasError ? Color.red : Color.gray)
+                    )
+                    .disableAutocorrection(true)
+                    .onChange(of: feedUrl) { _ in
+                        hasError = false
+                    }
+
+                if hasError {
+                    Text(errorText)
+                        .listRowSeparator(.hidden)
+                        .foregroundColor(.red)
+                        .font(.footnote)
+                        .padding(0)
                 }
 
-            if hasError {
-                Text(errorText)
-                    .listRowSeparator(.hidden)
-                    .foregroundColor(.red)
-                    .font(.footnote)
-                    .padding(0)
-            }
-
-            Button(action: startScanningFeed) {
-                HStack {
-                    Spacer()
-                    Text(L10n.AddNewFeed.scanButtonTitle)
-                        .padding(4)
-                    Spacer()
+                Button(action: startScanningFeed) {
+                    HStack {
+                        Spacer()
+                        Text(L10n.AddNewFeed.scanButtonTitle)
+                            .padding(4)
+                        Spacer()
+                    }
                 }
+                .buttonStyle(.borderedProminent)
+                .disabled(feedUrl.count < 3)
+                .listRowSeparator(.hidden)
             }
-            .buttonStyle(.borderedProminent)
-            .disabled(feedUrl.count < 3)
-            .listRowSeparator(.hidden)
 
             if isLoading {
                 HStack {
@@ -56,6 +60,13 @@ struct AddFeedView: View {
                     Spacer()
                 }
             }
+
+            List {
+                ForEach(foundRssFeeds, id: \.self) { item in
+                    Text(item)
+                }
+            }
+            .listStyle(.inset)
         }
         .navigationTitle(L10n.AddNewFeed.title)
         .navigationBarTitleDisplayMode(.inline)
@@ -66,18 +77,53 @@ struct AddFeedView: View {
             return
         }
 
+        foundRssFeeds = []
         isLoading = true
         hasError = false
         Networking.getRSSPageOfSite(by: url) { result in
-            isLoading = false
             switch result {
             case .failure(let error):
+                isLoading = false
                 hasError = true
                 errorText = error.localizedDescription
-                print(error)
             case .success(let htmlDocument):
-                print(htmlDocument)
+                parseHtmlDocumentToFindRssFeeds(htmlDocument)
             }
+        }
+    }
+
+    private func parseHtmlDocumentToFindRssFeeds(_ html: String) {
+        guard let doc = try? SwiftSoup.parse(html), let elements = try? doc.getAllElements() else {
+            hasError = true
+            errorText = "Cannot parse received document"
+            return
+        }
+
+        let group = DispatchGroup()
+        for element in elements {
+            guard element.hasAttr("href"), var link = try? element.attr("href") else {
+                continue
+            }
+
+            if let url = URL(string: link), url.host == nil {
+                link = feedUrl + link
+            }
+
+            guard let url = URL(string: link) else {
+                continue
+            }
+
+            group.enter()
+            Networking.detectRssFeed(by: url) { isRSS in
+                group.leave()
+                if isRSS {
+                    foundRssFeeds.append(url.absoluteString)
+                }
+            }
+        }
+
+        group.notify(queue: .main) {
+            isLoading = false
         }
     }
 }
